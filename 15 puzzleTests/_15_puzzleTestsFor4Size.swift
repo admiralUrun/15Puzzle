285949d7567d9be5b5cccaeafbaefb73991c4b04
Andrii <admiralurun@gmail.com>
2019-07-02 11:40:12 +0300

    reWork Some tests

--- 15 puzzleTests/_15_puzzleTestsFor4Size.swift
+++ 15 puzzleTests/_15_puzzleTestsFor4Size.swift
@@ -28,7 +28,6 @@ class _15_puzzleTestsFor4Size: XCTestCase {
     }
     
     func testMove() {
-        game.preapearForNewGame()
         let c = Logic.Coordinate.init(row: gameSize - 2, col: gameSize - 1)
         XCTAssertEqual(game.moves, 0)
         game.move(cell: c)
@@ -40,7 +39,6 @@ class _15_puzzleTestsFor4Size: XCTestCase {
     }
     
     func testGameEnd() {
-        game.preapearForNewGame()
         XCTAssertTrue(game.gameEnd())
         let c = Logic.Coordinate.init(row: gameSize - 2, col: gameSize - 1)
         game.move(cell: c)
@@ -48,7 +46,6 @@ class _15_puzzleTestsFor4Size: XCTestCase {
     }
     
     func testGetCoordinate() {
-        game.preapearForNewGame()
         let c = Logic.Coordinate.init(row: gameSize - 2, col: gameSize - 1)
         game.move(cell: c)
         
@@ -56,8 +53,49 @@ class _15_puzzleTestsFor4Size: XCTestCase {
         XCTAssertTrue(theSame(f: game.getCoordinateBy(tag: 12), s: game.getCoordinateBy(tag: 12)))
         
     }
-    // TODO: testFor getDirection and getDirectionForShuffling
+    
+    func testSwapTwo() {
+        XCTAssertTrue(game.gameEnd())
+        
+        game.swapTwo(firstCell: game.getCoordinateBy(tag: 11),
+                     secondCell: game.getCoordinateBy(tag: 12))
+        XCTAssertFalse(game.gameEnd(), "Swap didn't work")
+        
+        game.swapTwo(firstCell: game.getCoordinateBy(tag: 12),
+                     secondCell: game.getCoordinateBy(tag: 11))
+        XCTAssertTrue(game.gameEnd(), "Swap didn't work")
+    }
+    
+    func testGetDirection() {
+        let firstDirection = game.getDirection(to: game.getCoordinateBy(tag: 15))
+        var secondDirection = game.getDirection(to: game.getCoordinateBy(tag: 15))
+        XCTAssertEqual(firstDirection, secondDirection)
+        
+        secondDirection = game.getDirection(to: game.getCoordinateBy(tag: 12))
+        XCTAssertFalse(firstDirection == secondDirection, "getDirection isn't work correctly")
+    }
+    
+    func testGetDirectionForShuffling() {
+
+        let randomTag = Int.random(in: 0 ..< game.size * game.size)
+        let coordinate = game.getCoordinateBy(tag: randomTag)
+        
+        XCTAssertTrue(checkShuffleingDirection(s: game.getDirectionToShuffling(coordinate: coordinate )))
+    }
  
+    private func checkShuffleingDirection(s: Logic.ShuffelingSwap) -> Bool {
+        switch s.frist {
+        case .up:
+            return s.second == .down && s.firstTag != s.secondTag ? true : false
+        case .down:
+            return s.second == .up && s.firstTag != s.secondTag ? true : false
+        case .right:
+            return s.second == .left && s.firstTag != s.secondTag ? true : false
+        case .left:
+            return s.second == .right && s.firstTag != s.secondTag ? true : false
+        }
+    }
+    
     private func theSame(f: Logic.Coordinate, s: Logic.Coordinate) -> Bool {
         return f.row == s.row && f.col == s.col ? true : false
     }
